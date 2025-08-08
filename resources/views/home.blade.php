ce5da01ca348d038a9aaf0068f8e4f12176b04d3
Αποστόλης Μουστάκης <a.moustakis@netstudio.gr>
2025-08-08 17:47:49 +0300

    Webhook deploy test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="flex flex-col items-center justify-center mt-30">
-        <h1 class="text-white text-4xl font-bold">{{ __('Welcome to the AI Chatbot Builder') }}</h1>
+        <h1 class="text-white text-4xl font-bold">{{ __('Test webhook to the AI Chatbot Builder') }}</h1>
         <a
         href="#"
         class="text-xl inline-block px-6 py-2 rounded-full bg-emerald-400 text-black hover:bg-emerald-300 transition-colors mt-8">
