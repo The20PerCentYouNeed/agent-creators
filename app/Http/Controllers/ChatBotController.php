17cb5ed6db342592e173a8eaeca1f948e02114fc
Αποστόλης Μουστάκης <a.moustakis@netstudio.gr>
2025-11-26 21:35:06 +0200

    Fix controller

--- app/Http/Controllers/ChatBotController.php
+++ app/Http/Controllers/ChatBotController.php
@@ -244,8 +244,6 @@ class ChatBotController extends Controller
             $textContent = preg_replace('/\s+/', ' ', $textContent);
             $textContent = trim($textContent);
 
-            Log::info('Fetched URL content', ['url' => $url, 'content' => $textContent]);
-
             return $textContent ?: 'No text content found on this page.';
         }
         catch (\Exception $e) {
