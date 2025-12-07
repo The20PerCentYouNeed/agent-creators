1082889ca619cac1da3401941680b5e3fde1f86b
Αποστόλης Μουστάκης <a.moustakis@netstudio.gr>
2025-12-07 17:51:37 +0200

    decrease size

--- resources/views/components/chatbot.blade.php
+++ resources/views/components/chatbot.blade.php
@@ -208,7 +208,7 @@
             <!-- Thank You Message (after rating submitted) -->
             <div x-show="hasReachedLimit && (hasRated || ratingSubmitted)" class="text-center">
                 <div class="flex justify-center mb-2">
-                    <svg class="w-8 h-8 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <svg class="w-6 h-6 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
                     </svg>
                 </div>
