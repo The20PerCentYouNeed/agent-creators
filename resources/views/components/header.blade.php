8897886516b645451624b71c3f388e36d8248f43
Αποστόλης Μουστάκης <a.moustakis@netstudio.gr>
2025-12-19 18:15:32 +0200

    Test deploy

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,7 +5,7 @@
             <!-- Desktop Navigation -->
             <div class="hidden md:flex justify-between items-center py-[10px]">
                 <!-- Logo -->
-                <a href="{{ localized_route('home') }}" class="flex items-center gap-2">
+                <a href="{{ localized_route('home') }}" class="flex items-center">
                     <x-logo />
                     <picture>
                         <source
